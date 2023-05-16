0ec319a3586b2c12f20597bfb8d53026deef5383
joel <joel@chequer.io>
2023-05-16 18:31:55 +0900

    ImageCard 수정

--- src/components/SectionA/ImageCard/index.tsx
+++ src/components/SectionA/ImageCard/index.tsx
@@ -3,13 +3,13 @@ import * as S from './styled';
 type ImageCardType = {
   src: string;
   title: string;
-  description: string;
+  description: string | JSX.Element[];
 };
 
 const ImageCard = ({ src, title, description }: ImageCardType) => {
   return (
     <S.Wrapper>
-      <S.Image />
+      <S.Image src={src} />
       <S.Title>{title}</S.Title>
       <S.Description>{description}</S.Description>
     </S.Wrapper>
--- src/components/SectionA/ImageCard/styled.ts
+++ src/components/SectionA/ImageCard/styled.ts
@@ -1,18 +1,35 @@
 import styled from '@emotion/styled';
 
+import { mobile } from '@/styles/media-query';
+
 export const Wrapper = styled.div`
   background: ${({ theme }) => theme.colors.gray01};
   border-radius: 72px;
   padding: 32px;
   padding-bottom: 64px;
   width: 100%;
+
+  ${mobile} {
+    min-width: 320px;
+    min-height: 357px;
+    padding: 20px;
+    padding-bottom: 48px;
+    border-radius: 24px;
+  }
 `;
 
-export const Image = styled.div`
+export const Image = styled.img`
   width: 100%;
   height: 382px;
-  border-radius: 40px;
+  border-radius: 72px; // TODO: 확인방법 체크
   background-color: #fff; // TODO: 삭제
+
+  ${mobile} {
+    margin-left: 4px;
+    font-size: 18px;
+    line-height: 21px;
+    border-radius: 24px; // TODO: 확인방법 체크
+  }
 `;
 
 export const Title = styled.div`
@@ -20,6 +37,16 @@ export const Title = styled.div`
   font-family: 'Pretendard-ExtraBold';
   font-size: 28px;
   line-height: 33px;
+  margin-left: 24px;
+  font-weight: 800;
+
+  ${mobile} {
+    margin-top: 32px;
+    margin-left: 4px;
+    font-weight: 800;
+    font-size: 18px;
+    line-height: 21px;
+  }
 `;
 
 export const Description = styled.div`
@@ -27,4 +54,11 @@ export const Description = styled.div`
   font-size: 20px;
   line-height: 140%;
   color: ${({ theme }) => theme.colors.gray03};
+  margin-left: 24px;
+
+  ${mobile} {
+    margin-top: 16px;
+    margin-left: 4px;
+    font-size: 14px;
+  }
 `;
