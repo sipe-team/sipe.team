7145be2576314e92f0f789edc13a98fdc61e33a0
joel <joel@chequer.io>
2023-05-16 18:33:26 +0900

    JoinBanner 수정

--- src/components/SectionA/JoinBanner/index.tsx
+++ src/components/SectionA/JoinBanner/index.tsx
@@ -5,10 +5,10 @@ import * as S from './styled';
 const JoinBanner = () => {
   return (
     <S.Wrapper>
-      <S.Text>
-        <div>SIPE의 1기 멤버 현재 모집중!!</div>
-        <div>05/19 (금) ~ 06/02 (금)</div>
-      </S.Text>
+      <S.TextWrapper>
+        <S.Text>SIPE의 1기 멤버 현재 모집중!!</S.Text>
+        <S.Text>05/19 (금) ~ 06/02 (금)</S.Text>
+      </S.TextWrapper>
       <JoinButton size="m" />
     </S.Wrapper>
   );
