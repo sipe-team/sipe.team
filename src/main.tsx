38044012ff1660a65f71f380690ef8668ee87ddd
lee jeongmin <ljm991108@gmail.com>
2024-01-13 16:39:52 +0900

    router 추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,30 @@ import { ThemeProvider } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Recruit from './pages/Recruit';
+import Contact from './pages/Contact';
+import People from './pages/People';
+import Activity from './pages/Activity';
+
+const router = createBrowserRouter([
+  { path: '/', element: <Home /> },
+  { path: '/about', element: <About /> },
+  { path: '/recruit', element: <Recruit /> },
+  { path: '/people', element: <People /> },
+  { path: '/activity', element: <Activity /> },
+  { path: '/contact', element: <Contact /> },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/pages/Recruit.tsx
@@ -0,0 +1,3 @@
+export default function Recruit() {
+  return <div>Recruit</div>;
+}
