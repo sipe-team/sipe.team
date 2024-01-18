f169bcdd9f2e6c04aec259071b85ab190b2c8d46
Kim HunJin (Mos) <ysg01129@naver.com>
2024-01-18 23:12:09 +0900

    Infinite scroll 컴포넌트 개발 (#7)
    * intersection observer library 개발
    
    * Infinity scroll 컴포넌트 개발
      - 확장성을 위한 개선 필요
      - 디자인적 처리를 위한 개선 필요

--- src/components/common/InfiniteScroll/index.tsx
+++ src/components/common/InfiniteScroll/index.tsx
@@ -1,7 +1,32 @@
-type InfiniteScrollProps = React.ComponentProps<'div'> & {};
+import React from 'react';
+import { useRef } from 'react';
 
-const InfiniteScroll = ({ children }: InfiniteScrollProps) => {
-  return <div>{children}</div>;
+import { useInfiniteScroll } from '@/components/common/InfiniteScroll/hooks/useInfiniteScroll';
+
+import * as S from './styled';
+
+type InfiniteScrollProps = React.ComponentProps<'div'> & {
+  items: React.ReactNode[];
+};
+
+const InfiniteScroll = ({ items, children }: InfiniteScrollProps) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { elements, isEnd } = useInfiniteScroll(containerRef, items);
+
+  return (
+    <S.Wrapper>
+      {elements.map((element, index) => (
+        <React.Fragment key={index}>{element}</React.Fragment>
+      ))}
+      {!isEnd && (
+        <div
+          style={{ width: '100%', height: '10px' }}
+          className="flag"
+          ref={containerRef}
+        ></div>
+      )}
+    </S.Wrapper>
+  );
 };
 
 export default InfiniteScroll;
---
+++ src/libs/utils/tick.ts
@@ -0,0 +1,31 @@
+export const debounce = <T extends (...args: unknown[]) => unknown>(
+  func: T,
+  delay = 0
+) => {
+  let timer: ReturnType<typeof setTimeout>;
+
+  return (...args: Parameters<T>): ReturnType<T> => {
+    let result: any;
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      result = func(...args);
+    }, delay);
+    return result;
+  };
+};
+
+export const throttle = <T extends unknown[]>(
+  fun: (...params: T) => unknown,
+  delay: number
+) => {
+  let flag = true;
+  return (...params: T) => {
+    if (flag) {
+      fun(...params);
+      flag = false;
+      setTimeout(() => (flag = true), delay);
+    }
+  };
+};
