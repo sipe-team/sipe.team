e3290cf348177741e853c78d72d836b84465a1e8
KimHunJin <ysg01129@naver.com>
2024-01-22 18:21:45 +0900

    Active Card 작업

---
+++ src/pages/Activity/components/ActiveCard.tsx
@@ -0,0 +1,58 @@
+import { ReactComponent as UserIcon } from '@/assets/ico_user.svg';
+import CardWrapper from '@/components/CardWrapper';
+import Image from '@/components/Image';
+import { StyledH5 } from '@/styles/common';
+
+import * as S from './styled';
+import useDeviceType from "@/hook/useDeviceType";
+
+type ActiveCardProps = React.ComponentProps<'div'> & {
+  thumbnail?: string;
+  contentTitle: string;
+  contentBody: string;
+  userName: string;
+  link: string;
+  createDate: string;
+};
+
+export const ActiveCard = ({
+  thumbnail,
+  contentTitle,
+  contentBody,
+  userName,
+  createDate,
+  link,
+  className,
+}: ActiveCardProps) => {
+  const handleClick = () => {
+    window.open(link);
+  };
+
+  const { isMobile } = useDeviceType();
+
+  return (
+    <CardWrapper
+      type="CONTENT"
+      minHeight={isMobile ? 333 : 252}
+      onClick={handleClick}
+      className={className}
+    >
+      <S.Wrapper isMobile={isMobile}>
+        <Image className="thumbnail" src={thumbnail} />
+        <S.ContentsWrapper isMobile={isMobile}>
+          <section className="contents">
+            <StyledH5 className="title">{contentTitle}</StyledH5>
+            <p className="body">{contentBody}</p>
+          </section>
+          <section className="poster-user-info">
+            <div className="user-wrapper">
+              <UserIcon className="user-icon" />
+              <label className="user-name">{userName}</label>
+            </div>
+            <label className="create-date">{createDate}</label>
+          </section>
+        </S.ContentsWrapper>
+      </S.Wrapper>
+    </CardWrapper>
+  );
+};
--- src/styles/common.ts
+++ src/styles/common.ts
@@ -20,34 +20,40 @@ export const StyledH1 = styled.h1`
   font-weight: ${({ theme }) => theme.fontWeight.black};
   font-size: 48px;
   letter-spacing: 2%;
+  line-height: 28px;
 `;
 
 export const StyledH2 = styled.h2`
   font-weight: ${({ theme }) => theme.fontWeight.extraBold};
   font-size: 36px;
   letter-spacing: 2%;
+  line-height: 28px;
 `;
 
 export const StyledH3 = styled.h3`
   font-weight: ${({ theme }) => theme.fontWeight.bold};
   font-size: 28px;
   letter-spacing: 2%;
+  line-height: 28px;
 `;
 
 export const StyledH4 = styled.h4`
   font-weight: ${({ theme }) => theme.fontWeight.bold};
   font-size: 24px;
   letter-spacing: 2%;
+  line-height: 28px;
 `;
 
 export const StyledH5 = styled.h5`
   font-weight: ${({ theme }) => theme.fontWeight.bold};
   font-size: 20px;
   letter-spacing: 2%;
+  line-height: 28px;
 `;
 
 export const StyledH6 = styled.h6`
   font-weight: ${({ theme }) => theme.fontWeight.semiBold};
   font-size: 17px;
   letter-spacing: 2%;
+  line-height: 28px;
 `;
