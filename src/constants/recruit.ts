bbff17d4c421524b88470c1e417a68d1b8eef02a
cindy <cindy@bemyfriends.com>
2024-08-25 15:26:29 +0900

    feat: recruit 내용 업데이트

--- src/constants/recruit.ts
+++ src/constants/recruit.ts
@@ -3,55 +3,55 @@ export const Applicants = [
     text: '격주 토요일 오후 2시 ~ 6시에 진행되는 정규 활동에 성실하게 참여할 수 있는',
   },
   {
-    text: '관심사가 비슷한 사람들과 함께 기술을 학습하고 공유하며 성장하고 싶은',
+    text: '본인이 가진 지식을 주변 사람에게 나누는 데에 거리낌이 없는',
   },
   { text: '다양한 경험을 가진 사람들과 폭넓고 깊이 있는 대화를 나누고 싶은' },
-  { text: 'SW 개발 경력을 가진 현직 개발자' },
+  { text: '기술에 관심있는 모든 개발자' },
 ];
 
 export const InActivity = [
   {
-    recurring_date: '1회차 (03.16)',
+    recurring_date: '1회차 (10.12)',
     text: 'OT',
     badge: '',
   },
   {
-    recurring_date: '2회차 (03.30)',
-    text: '사이프챗',
-    badge: '1차 미션 시작',
-  },
-  {
-    recurring_date: '3회차 (04.13)',
-    text: '사이데이션',
+    recurring_date: '2회차 (10.26)',
+    text: 'MT',
     badge: '',
   },
   {
-    recurring_date: '4회차 (04.27)',
-    text: '사담콘',
+    recurring_date: '3회차 (11.09)',
+    text: '정규 활동',
     badge: '',
   },
   {
-    recurring_date: '5회차 (05.11)',
+    recurring_date: '4회차 (11.23)',
+    text: '정규 활동',
+    badge: '',
+  },
+  {
+    recurring_date: '5회차 (12.07)',
     text: '1차 미션 발표',
     badge: '',
   },
   {
-    recurring_date: '6회차 (05.25)',
-    text: '사이프톤',
+    recurring_date: '6회차 (12.21)',
+    text: '사담콘',
     badge: '2차 미션 시작',
   },
   {
-    recurring_date: '7회차 (06.08)',
-    text: '사이데이션',
-    badge: '',
-  },
-  {
-    recurring_date: '8회차 (06.22)',
+    recurring_date: '7회차 (01.04)',
     text: '내친소',
     badge: '',
   },
   {
-    recurring_date: '9회차 (07.06)',
+    recurring_date: '8회차 (01.218)',
+    text: '사이프톤',
+    badge: '',
+  },
+  {
+    recurring_date: '9회차 (02.01)',
     text: '2차 미션 발표',
     badge: '정규 활동 종료',
   },
@@ -60,28 +60,28 @@ export const InActivity = [
 export const CardList = [
   {
     title: '서류 접수',
-    processDate: '02.07(수) ~ 02.17(토)',
+    processDate: '09.14(토) ~ 09.22(일)',
     subTitle: '23:59:59 마감',
   },
   {
-    title: '서류 결과 발표',
-    processDate: '02.21(수)',
+    title: '서류 합격자 발표',
+    processDate: '09.27(금)',
     subTitle: '오후 8시 예정',
   },
   {
     title: '오프라인 인터뷰',
-    processDate: '03.01(금) ~ 03.03(일)',
+    processDate: '10.03(목) ~ 10.06(일)',
     subTitle: '서류합격자 개별연락',
   },
 
   {
-    title: '최종 합격 발표',
-    processDate: '03.06(수)',
+    title: '최종 합격자 발표',
+    processDate: '10.08(화)',
     subTitle: '오후 8시 예정',
   },
   {
     title: '정규 활동 시작',
-    processDate: '03.16(토)',
+    processDate: '10.12(토)',
     subTitle: 'OT 진행',
   },
 ];
