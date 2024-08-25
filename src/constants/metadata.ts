1ca616ad766912773b2069a6b9bf2f77a3461d4d
saseungmin <dbd02169@naver.com>
2024-08-26 01:03:15 +0900

    fix: build error in metadata url

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -4,7 +4,8 @@ const title = 'SIPE';
 const description =
   '기술에 관심있는 모든 개발자들이 모여 함께 학습하고 소통하는 온오프라인 커뮤니티';
 const images = ['/assets/logos/og-image.png'];
-const metadataBase = new URL(process.env.VERCEL_URL);
+const defaultURL = `https://${process.env.VERCEL_URL}`;
+const metadataBase = new URL(defaultURL);
 
 export const DEFAULT_METADATA: Metadata = {
   metadataBase,
@@ -13,7 +14,7 @@ export const DEFAULT_METADATA: Metadata = {
   openGraph: {
     title,
     description,
-    url: process.env.VERCEL_URL,
+    url: defaultURL,
     images,
   },
   twitter: {
