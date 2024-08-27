42f16c66c52d98676349042f3e874e0a5ca3d721
saseungmin <dbd02169@naver.com>
2024-08-27 16:16:59 +0900

    chore: seo 세팅 (favicon, manifest, robot, sitemap)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,21 @@ export const metadata = DEFAULT_METADATA;
 function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko" className={montserrat.className}>
+      <head>
+        <link rel="mask-icon" href="/assets/safari-pinned-tab.svg" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/favicon-16x16.png"
+        />
+      </head>
       <body>
         <ClientProvider>{children}</ClientProvider>
         <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} />
