2069f3dd65fd168c3282ea9b0c74df23ca8c2688
Harang <dbd02169@naver.com>
2024-09-14 22:42:18 +0900

    refactor: accordion 하나의 아이템만 열리도록 state 관리 변경 (#83)

--- src/components/common/accordion/AccordionItem/index.tsx
+++ src/components/common/accordion/AccordionItem/index.tsx
@@ -2,41 +2,41 @@
 
 import clsx from 'clsx';
 import { AnimatePresence, motion } from 'framer-motion';
-import { useState } from 'react';
 
 import { AccordionArrowIcon } from '@/assets/icons';
 
 import styles from './index.module.scss';
 
-type AccordionProps = {
-  question: string;
-  answer: string;
+export type AccordionItemProps = {
+  title: string;
+  value: string;
+  onClick?: () => void;
+  isActive?: boolean;
 };
 
-function Accordion({ question, answer }: AccordionProps) {
-  const [open, toggle] = useState(false);
-
+function AccordionItem({
+  title,
+  value,
+  onClick,
+  isActive,
+}: AccordionItemProps) {
   return (
     <div className={styles.wrapper}>
-      <motion.button
-        type="button"
-        className={styles.question}
-        onClick={() => toggle(!open)}
-      >
-        {question}
-        <div className={clsx(styles.arrow, { [styles.isOpen]: open })}>
+      <motion.button type="button" className={styles.title} onClick={onClick}>
+        {title}
+        <div className={clsx(styles.arrow, { [styles.isOpen]: isActive })}>
           <AccordionArrowIcon className={styles.arrowIcon} />
         </div>
       </motion.button>
       <AnimatePresence initial={false}>
-        {open && (
+        {isActive && (
           <motion.div
             initial={{ height: 0 }}
             animate={{ height: 'auto' }}
             exit={{ height: 0 }}
             transition={{ type: 'spring', duration: 0.4, bounce: 0 }}
           >
-            <div className={styles.answer}>{answer}</div>
+            <div className={styles.value}>{value}</div>
           </motion.div>
         )}
       </AnimatePresence>
@@ -44,4 +44,4 @@ function Accordion({ question, answer }: AccordionProps) {
   );
 }
 
-export default Accordion;
+export default AccordionItem;
