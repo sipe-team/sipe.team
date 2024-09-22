b97a489051071a63a221d923f738ed0cee3ece2b
hy57in <gywls00100@gmail.com>
2024-09-23 00:10:56 +0900

    feat: Summary data 업데이트

--- src/components/pages/Home/components/SummaryCard/index.tsx
+++ src/components/pages/Home/components/SummaryCard/index.tsx
@@ -2,9 +2,9 @@ import SummaryCardItem from './components/SummaryCardItem';
 import styles from './index.module.scss';
 
 const summaryData = [
-  { title: '누적 지원자 수', value: '300+' },
-  { title: '총 참여자 수', value: '100+' },
-  { title: '누적 미션 수', value: '50+' },
+  { title: '누적 지원자 수', value: '400+' },
+  { title: '총 참여자 수', value: '60+' },
+  { title: '누적 미션 수', value: '30+' },
 ];
 
 function SummaryCard() {
