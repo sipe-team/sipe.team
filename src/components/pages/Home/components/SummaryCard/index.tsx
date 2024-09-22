5874d5c50479238184e8253ed9d4a9b7ded1f006
hy57in <gywls00100@gmail.com>
2024-09-22 14:45:55 +0900

    chore: data 수정

--- src/components/pages/Home/components/SummaryCard/index.tsx
+++ src/components/pages/Home/components/SummaryCard/index.tsx
@@ -2,7 +2,7 @@ import SummaryCardItem from './components/SummaryCardItem';
 import styles from './index.module.scss';
 
 const summaryData = [
-  { title: '누적 지원 수', value: '300+' },
+  { title: '누적 지원자 수', value: '300+' },
   { title: '총 참여자 수', value: '100+' },
   { title: '누적 미션 수', value: '50+' },
 ];
