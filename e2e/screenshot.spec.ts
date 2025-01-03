f7996b3aaba0d2b99e93261d4237ca119f81e659
noahluftyang <euihyun.yang.9x@gmail.com>
2025-01-03 21:40:00 +0900

    feat: add automated screenshots for visual comparisons

---
+++ e2e/screenshot.spec.ts
@@ -0,0 +1,46 @@
+import { expect, test } from '@playwright/test';
+
+test('메인 페이지', async ({ page }) => {
+  await page.goto('/', { waitUntil: 'networkidle' });
+
+  await expect(page).toHaveScreenshot({
+    fullPage: true,
+    maxDiffPixelRatio: 0.2,
+  });
+});
+
+test('사이프 소개 페이지', async ({ page }) => {
+  await page.goto('/about', { waitUntil: 'networkidle' });
+
+  await expect(page).toHaveScreenshot({
+    fullPage: true,
+    maxDiffPixelRatio: 0.2,
+  });
+});
+
+test('사이퍼 모집 페이지', async ({ page }) => {
+  await page.goto('/recruit', { waitUntil: 'networkidle' });
+
+  await expect(page).toHaveScreenshot({
+    fullPage: true,
+    maxDiffPixelRatio: 0.2,
+  });
+});
+
+test('사이퍼 소개 페이지', async ({ page }) => {
+  await page.goto('/people', { waitUntil: 'networkidle' });
+
+  await expect(page).toHaveScreenshot({
+    fullPage: true,
+    maxDiffPixelRatio: 0.2,
+  });
+});
+
+test('사이퍼 활동 페이지', async ({ page }) => {
+  await page.goto('/activity', { waitUntil: 'networkidle' });
+
+  await expect(page).toHaveScreenshot({
+    fullPage: true,
+    maxDiffPixelRatio: 0.2,
+  });
+});
