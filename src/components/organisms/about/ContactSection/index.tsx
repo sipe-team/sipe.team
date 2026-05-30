f1316d0a7f53bea29a4aa3d5ac75e07758c9f59f
KYBee <kyb3634@naver.com>
2026-05-30 18:52:04 +0900

    style(flex): format contact section migration

--- src/components/organisms/about/ContactSection/index.tsx
+++ src/components/organisms/about/ContactSection/index.tsx
@@ -21,12 +21,7 @@ function ContactSection() {
           더 궁금한 질문이 있거나 후원을 원하신다면 언제든 연락해 주세요!
         </div>
         <div className={styles.content}>
-          <Flex
-            align="center"
-            asChild
-            className={styles.contentBox}
-            gap="12px"
-          >
+          <Flex align="center" asChild className={styles.contentBox} gap="12px">
             <ExternalLink
               href="mailto:sipe.team@gmail.com"
               withTextUnderline={false}
