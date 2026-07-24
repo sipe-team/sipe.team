ea030ad3b741e996831a4006a990130f49bc3232
KYBee <kyb3634@naver.com>
2026-07-24 16:55:47 +0900

    refactor: migrate Activity responsive Flex layouts

--- src/components/organisms/activity/ActiveCard/index.tsx
+++ src/components/organisms/activity/ActiveCard/index.tsx
@@ -26,77 +26,79 @@ function ActiveCard({
   link,
 }: ActiveCardProps) {
   return (
-    <ExternalLink
-      href={link}
-      withTextUnderline={false}
-      className={styles.activeCardWrapper}
-    >
-      <Image
-        className={styles.thumbnail}
-        src={thumbnail}
-        fill
-        height={220}
-        objectFit="cover"
-        alt="thumbnail"
-        sizes="(max-width: 1060px) 50vw, 33vw"
-      />
-      <Flex
-        asChild
-        className={styles.contentsWrapper}
-        direction="column"
-        justify="space-between"
+    <Flex asChild direction={{ sm: 'column', md: 'row' }}>
+      <ExternalLink
+        href={link}
+        withTextUnderline={false}
+        className={styles.activeCardWrapper}
       >
-        <article>
-          <section className={styles.contents}>
-            <Typography
-              color={color.white}
-              className={styles.title}
-              size={20}
-              weight="bold"
-            >
-              {contentTitle}
-            </Typography>
-            <Typography className={styles.body} size={14}>
-              {contentBody}
-            </Typography>
-          </section>
-          <Flex
-            asChild
-            align="center"
-            className={styles.posterUserInfo}
-            justify="space-between"
-          >
-            <section>
-              <Flex asChild align="center" gap="12px" justify="center">
-                <div className={styles.userWrapper}>
-                  {profile ? (
-                    <Image
-                      src={profile}
-                      alt="user profile"
-                      width={32}
-                      height={32}
-                      className={styles.userIcon}
-                    />
-                  ) : (
-                    <UserIcon className={styles.userIcon} />
-                  )}
-                  <Typography color={color.white} size={14}>
-                    {userName}
-                  </Typography>
-                </div>
-              </Flex>
+        <Image
+          className={styles.thumbnail}
+          src={thumbnail}
+          fill
+          height={220}
+          objectFit="cover"
+          alt="thumbnail"
+          sizes="(max-width: 1060px) 50vw, 33vw"
+        />
+        <Flex
+          asChild
+          className={styles.contentsWrapper}
+          direction="column"
+          justify="space-between"
+        >
+          <article>
+            <section className={styles.contents}>
               <Typography
-                className={styles.createDate}
-                size={12}
-                weight="semiBold"
+                color={color.white}
+                className={styles.title}
+                size={20}
+                weight="bold"
               >
-                {createDate}
+                {contentTitle}
+              </Typography>
+              <Typography className={styles.body} size={14}>
+                {contentBody}
               </Typography>
             </section>
-          </Flex>
-        </article>
-      </Flex>
-    </ExternalLink>
+            <Flex
+              asChild
+              align="center"
+              className={styles.posterUserInfo}
+              justify="space-between"
+            >
+              <section>
+                <Flex asChild align="center" gap="12px" justify="center">
+                  <div className={styles.userWrapper}>
+                    {profile ? (
+                      <Image
+                        src={profile}
+                        alt="user profile"
+                        width={32}
+                        height={32}
+                        className={styles.userIcon}
+                      />
+                    ) : (
+                      <UserIcon className={styles.userIcon} />
+                    )}
+                    <Typography color={color.white} size={14}>
+                      {userName}
+                    </Typography>
+                  </div>
+                </Flex>
+                <Typography
+                  className={styles.createDate}
+                  size={12}
+                  weight="semiBold"
+                >
+                  {createDate}
+                </Typography>
+              </section>
+            </Flex>
+          </article>
+        </Flex>
+      </ExternalLink>
+    </Flex>
   );
 }
 
